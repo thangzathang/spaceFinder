2e77d5ebee34c62919c9744e952003eaccb90be1
Thang Za Thang <Thangzathang@gmail.com>
2024-12-08 15:17:59 +1100

    launcher test code for spaces get and scan commands

--- test/launcher.ts
+++ test/launcher.ts
@@ -18,19 +18,51 @@
     );
 */
 
-// 3. Node
-// Run in this launche file with ts node
-import { handler } from "../src/services/spaces/handler";
+// 3. Test POST with Node ts node
+// NOTE: You may need to install ts-node globally. Use `npm install -g ts-node`
+// Run in this launcher file with ts node
+/*
+  import { handler } from "../src/services/spaces/handler";
 
+  process.env.TABLE_NAME = "SpaceTable-0aec7385babb";
+  process.env.AWS_REGION = "eu-west-1";
+
+  handler(
+    {
+      httpMethod: "POST",
+      body: JSON.stringify({
+        location: "Sydney",
+      }),
+    } as any,
+    {} as any
+  );
+*/
+
+// 4. Test GET with ts-node
+/*
+  import { handler } from "../src/services/spaces/handler";
+  process.env.TABLE_NAME = "SpaceTable-0aec7385babb";
+  process.env.AWS_REGION = "eu-west-1";
+
+  handler(
+    {
+      httpMethod: "GET",
+    } as any,
+    {} as any
+  );
+*/
+
+// 5. Test GET with string query with ts-node
+import { handler } from "../src/services/spaces/handler";
 process.env.TABLE_NAME = "SpaceTable-0aec7385babb";
 process.env.AWS_REGION = "eu-west-1";
 
 handler(
   {
-    httpMethod: "POST",
-    body: JSON.stringify({
-      location: "Sydney",
-    }),
+    httpMethod: "GET",
+    queryStringParameters: {
+      id: "cbf74253-fbf1-4833-9693-067bc4835be6",
+    },
   } as any,
   {} as any
 );
